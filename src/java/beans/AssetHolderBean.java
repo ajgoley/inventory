44b43521fd28986c14169559a850968783f6f1e2
khwang18c <khwang18c@gmail.com>
2013-08-07 12:14:24 -0400

    new page added

--- src/java/beans/AssetHolderBean.java
+++ src/java/beans/AssetHolderBean.java
@@ -4,8 +4,12 @@
  */
 package beans;
 
+import entities.AssetHolder;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import sessions.AssetHolderFacade;
 
 /**
  *
@@ -15,9 +19,35 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class AssetHolderBean {
 
-    /**
-     * Creates a new instance of AssetHolderBean
-     */
-    public AssetHolderBean() {
+  @EJB
+  private AssetHolderFacade assetHolderFacade;
+    
+  private List<AssetHolder> filteredAssetHolders;
+  private List<AssetHolder> assetHolderDisplayTable;
+
+    /**Getters and Setters**/
+    public List<AssetHolder> getFilteredAssetHolders() {
+        return filteredAssetHolders;
     }
+
+    public void setFilteredAssetHolders(List<AssetHolder> filteredAssetHolders) {
+        this.filteredAssetHolders = filteredAssetHolders;
+    }
+
+    public List<AssetHolder> getAssetHolderDisplayTable() {
+        
+          if(assetHolderDisplayTable == null){
+           assetHolderDisplayTable = assetHolderFacade.findAll(); 
+        }
+        assetHolderFacade.updateTable();
+        
+        return assetHolderDisplayTable;
+    }
+
+    public void setAssetHolderDisplayTable(List<AssetHolder> assetHolderDisplayTable) {
+        this.assetHolderDisplayTable = assetHolderDisplayTable;
+    }
+
+    
+    
 }
