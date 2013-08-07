8f97234f2f4ff26a4be158996dfdf7ad16023919
khwang18c <khwang18c@gmail.com>
2013-08-07 15:16:10 -0400

    equipment facade

--- src/java/beans/AssetHolderBean.java
+++ src/java/beans/AssetHolderBean.java
@@ -7,9 +7,13 @@ package beans;
 import entities.AssetHolder;
 import entities.Equipment;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import sessions.AssetHolderFacade;
 import sessions.EquipmentFacade;
 
@@ -21,6 +25,9 @@ import sessions.EquipmentFacade;
 @RequestScoped
 public class AssetHolderBean {
 
+    
+  private static Logger log = Logger.getLogger("AssetHolderBean");  
+    
   @EJB
   private AssetHolderFacade assetHolderFacade;
   
@@ -28,10 +35,12 @@ public class AssetHolderBean {
   private EquipmentFacade equipmentFacade;
     
   private List<AssetHolder> filteredAssetHolders;
-  private AssetHolder selectedAssetHolder;
-  private List<Equipment> assetHolderEquipments;
+  private AssetHolder selectedAssetHolder = new AssetHolder();
   
+  //private List<Equipment> assetHolderEquipment;
+  private List<Equipment> filteredEquipment;
   
+  private String assetHolder_id;
   
   //Table in AssetHolder.xhtml
   private List<AssetHolder> assetHolderDisplayTable; 
@@ -67,24 +76,58 @@ public class AssetHolderBean {
         this.selectedAssetHolder = selectedAssetHolder;
     }
 
-    
-    
-    public List<Equipment> getAssetHolderEquipments() {
-        
-         if(assetHolderEquipments != null){
-              assetHolderEquipments = equipmentFacade.findAssetHolderEquipment(selectedAssetHolder.getAssetHolderId());
-          }
-        
-        return assetHolderEquipments;
+    public List<Equipment> getFilteredEquipment() {
+        return filteredEquipment;
     }
 
-    public void setAssetHolderEquipments(List<Equipment> assetHolderEquipments) {
-        this.assetHolderEquipments = assetHolderEquipments;
+    public void setFilteredEquipment(List<Equipment> filteredEquipment) {
+        this.filteredEquipment = filteredEquipment;
+    }
+
+//    public List<Equipment> getAssetHolderEquipment() {
+//        log.info("asset holder equipment called");
+//         if(assetHolderEquipment == null){ 
+//              assetHolderEquipment = equipmentFacade.findAssetHolderEquipment(selectedAssetHolder.getAssetHolderId());
+//          }
+//        
+//        return assetHolderEquipment;
+//    }
+//
+//    public void setAssetHolderEquipments(List<Equipment> assetHolderEquipments) {
+//        this.assetHolderEquipment = assetHolderEquipments;
+//    }
+
+    public String getAssetHolder_id() {
+        return assetHolder_id;
+    }
+
+    public void setAssetHolder_id(String assetHolder_id) {
+        this.assetHolder_id = assetHolder_id;
     }
     
     
+    /**Methods**/
     
+    private boolean isNullOrEmpty(String value) {
+        if (value == null || value.length() == 0) {
+            return true;
+        }
+        return false;
+    }
     
+    @PostConstruct
+    public void init(){
+          Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+	 assetHolder_id = params.get("asset_holder_id");  
+         
+         if(!isNullOrEmpty(assetHolder_id)){
+             //log.info("asset id = "+ assetHolder_id);
+             
+             selectedAssetHolder = assetHolderFacade.findBySocialSecruity(assetHolder_id);
+             
+             //log.info(selectedAssetHolder.getFullName());
+         }
+    }
 
     
     
