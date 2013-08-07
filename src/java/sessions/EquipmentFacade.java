b17bf2c87254e81947c7b5336fa0d407a67b67f7
khwang18c <khwang18c@gmail.com>
2013-08-07 12:35:19 -0400

    equipment facade

--- src/java/sessions/EquipmentFacade.java
+++ src/java/sessions/EquipmentFacade.java
@@ -5,9 +5,11 @@
 package sessions;
 
 import entities.Equipment;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -26,5 +28,20 @@ public class EquipmentFacade extends AbstractFacade<Equipment> {
     public EquipmentFacade() {
         super(Equipment.class);
     }
+    
+    
+    //Method used in AssetHolderBean
+    public List<Equipment> findAssetHolderEquipment(int assetHolderId) {
+      StringBuilder queryString = new StringBuilder();
+
+      queryString.append("SELECT e from Equipment e ");
+      queryString.append("WHERE e.employeeId.employeeId = ");
+      queryString.append(assetHolderId);
+
+      TypedQuery<Equipment> query = em.createQuery(queryString.toString(), Equipment.class);
+
+      return query.getResultList();
+
+  }
     
 }
