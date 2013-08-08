a255225278cbcdcf7424c8fc7addc29f092ca96b
Internship <Internship@intelitrac10-PC>
2013-08-08 10:16:12 -0400

    inventory page and navigation update

---
+++ src/java/beans/inventoryBean.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import entities.Equipment;
+import java.util.List;
+import javax.ejb.EJB;
+import sessions.EquipmentFacade;
+
+/**
+ *
+ * @author Internship
+ */
+@ManagedBean
+@RequestScoped
+public class inventoryBean {
+    @EJB
+    EquipmentFacade equipmentFacade = new EquipmentFacade();
+
+    /**
+     * Creates a new instance of inventoryBean
+     */
+    public inventoryBean() {
+    }
+
+    public List<Equipment> getTheEquipemntList() {
+        return equipmentFacade.findAll();
+    }
+}
